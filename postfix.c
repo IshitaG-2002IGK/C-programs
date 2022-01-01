fece6a0caee673bf8aa2bdf3f0382de5378e033e
ishita <ishitagops12@gmail.com>
2022-01-01 17:23:15 +0530

    final changes made to postfix evaluation

--- postfix.c
+++ postfix.c
@@ -48,7 +48,64 @@ int main()
         }
     }
 
-    printf ("\n The original expression is :%s", p) ;
-    printf ("The evaluated expression is :%d", s[top]) ;
-
+    printf("\n Given Postfix Expn: %s\n",p);
+    printf("\n Result after Evaluation: %d\n",s[top]);
+ 
 }
+
+
+
+
+
+
+
+
+// #define SIZE 50
+// #include <ctype.h>
+// #include <stdio.h>
+
+// int s[SIZE];
+// int top=-1;
+// int push(int elem)
+// {
+// s[++top]=elem;
+// }
+// int pop()
+// {
+// return(s[top--]);
+// }
+// int main()
+// {
+// char pofx[50],ch;
+// int i=0,op1,op2;
+// printf("<-----Stack Application: Evaluating Postfix Expression----->\n");
+// printf("\n\nEnter the Postfix Expression : ");
+// scanf("%s",pofx);
+// while( (ch=pofx[i++]) != '\0')
+// {
+//  if(isdigit(ch)) 
+// push(ch-'0');
+//  else
+//  {
+//  op2=pop();
+//  op1=pop();
+//  switch(ch)
+//  {
+//  	case '+':
+//             push(op1+op2);
+//             break;
+//     case '-'
+//             push(op1-op2);
+//             break;
+//  	case '*':
+//             push(op1*op2);
+//             break;
+//  	case '/':
+//             push(op1/op2);
+//             break;
+//  }
+// }
+// }
+// printf("\n Given Postfix Expn: %s\n",pofx);
+// printf("\n Result after Evaluation: %d\n",s[top]);
+// }
