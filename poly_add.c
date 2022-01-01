e989439bc946e4ca0601a2741a68388cc14966d8
ishita <ishitagops12@gmail.com>
2022-01-01 20:23:16 +0530

    polynomial addition

--- poly_add.c
+++ poly_add.c
@@ -7,98 +7,98 @@ struct link
 {
     int coeff;
     int pow;
-    struct link * next;
+    struct link *next;
 };
 
-struct link*poly1=NULL, *poly2=NULL, *poly2=NULL;
+struct link *poly1=NULL, *poly2=NULL, *poly=NULL;
 
-void create(struct link* node)
+void create(struct link *node)
 {
     char ch;
     do
     {
         printf("Enter the coefficient value:\n");
-        scanf("%d",&node -> coeff);
+        scanf("%d",&node->coeff);
         printf("Enter the power value:\n");
-        scanf("%d",&node -> pow);
-        node -> next = (struct link*)malloc(sizeof(struct link));
-        node = node -> next;
-        node -> next = NULL;
-        printf("\n Do you wish to continue ?");
+        scanf("%d",&node->pow);
+        node->next = (struct link*)malloc(sizeof(struct link));
+        node = node->next;
+        node->next = NULL;
+        printf("\n Do you wish to continue(y/n) :");
         ch = getch();
 
-    }while (ch ='y' || ch ='Y');
+    }while (ch =='y' || ch =='Y');
 }
 
-void show(struct link* node)
+void show(struct link *node)
 {
-    while(node -> next != NULL)
+    while(node->next != NULL)
     {
-        printf("%d x ^ %d", node -> coff, node -> pow);
-        node =node -> next;
-        if(node -> next != NULL)
-            print("+");
+        printf("%d x ^ %d", node->coeff, node->pow);
+        node =node->next;
+        if(node->next != NULL)
+            printf("+");
 
     }
 }
 
-void poly_add(struct link*poly1, struct link*poly2, struct link*poly)
+void poly_add(struct link *poly1, struct link *poly2, struct link *poly)
 {
     while(poly1 -> next && poly2 ->next)
     {
-        if( poly1 -> pow > poly2 ->pow)
+        if( poly1->pow > poly2->pow)
         {
-            poly -> pow = poly1 ->pow ;
-            poly -> coeff = poly1 ->coeff ;
-            poly1 = poly1 -> pow;
+            poly->pow = poly1->pow ;
+            poly->coeff = poly1->coeff ;
+            poly1 = poly1->next;
         }
         else if( poly1 -> pow < poly2 ->pow)
         {
-            poly -> pow = poly2 ->pow ;
-            poly -> coeff = poly2 ->coeff ;
-            poly2 = poly2 -> pow;
+            poly->pow = poly2->pow ;
+            poly->coeff = poly2->coeff ;
+            poly2 = poly2->next;
         }
         else
         {
-            poly -> pow = poly1 ->pow;
-            poly -> coeff = poly1 -> coeff + poly2 -> coeff;
-            poly1 = poly1 -> next;
-            poly2 = poly2 -> next;
+            poly->pow = poly1->pow;
+            poly->coeff = poly1->coeff + poly2->coeff;
+            poly1 = poly1->next;
+            poly2 = poly2->next;
         }
 
-        poly -> next = (struct link*)malloc(sizeof(struct link))
-        poly = poly -> next;
-        poly -> next = NULL;
+        poly -> next = (struct link *)malloc(sizeof(struct link));
+        poly = poly->next;
+        poly->next = NULL;
 
     }
     while(poly1 -> next || poly2 ->next)
     {
         if(poly1 -> next)
         {
-            poly -> pow = poly1 ->pow ;
-            poly -> coeff = poly1 ->coeff ;
-            poly1 = poly1 -> pow;
+            poly->pow = poly1->pow ;
+            poly->coeff = poly1->coeff ;
+            poly1 = poly1->next;
         }
         if(poly2 -> next)
         {
-            poly -> pow = poly2 ->pow ;
-            poly -> coeff = poly2 ->coeff ;
-            poly2 = poly2 -> pow;
+            poly->pow = poly2->pow ;
+            poly->coeff = poly2->coeff ;
+            poly2 = poly2->next;
         }
-        poly -> next = (struct link*)malloc(sizeof(struct link));
-        poly = poly -> next;
-        poly -> next = NULL;
+        poly -> next = (struct link *)malloc(sizeof(struct link));
+        poly = poly->next;
+        poly->next = NULL;
 
     }
-
+}
 void main()
 {
     char ch;
     do
     {
-        poly1 = (struct link*)malloc(sizeof(struct link));
-        poly2 = (struct link*)malloc(sizeof(struct link));
-        poly  = (struct link*)malloc(sizeof(struct link));
+        poly1 = (struct link *)malloc(sizeof(struct link));
+        poly2 = (struct link *)malloc(sizeof(struct link));
+        poly  = (struct link *)malloc(sizeof(struct link));
         printf("Enter the 1st number:\n");
         create(poly1);
         printf("Enter the 2nd number:\n");
@@ -111,7 +111,7 @@ void main()
         printf("Additon of the polynomials are :\n");
         show(poly);
         printf("Wish to add more numbers :");
-        ch = gecth();
+        ch = getch();
 
-    }while (ch ='y' || ch ='Y');
+    }while (ch =='y' || ch =='Y');
 }
