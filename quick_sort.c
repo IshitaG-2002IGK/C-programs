13b319213113893804c839e870f2e41184ea07f9
ishita <ishitagops12@gmail.com>
2022-01-01 17:17:13 +0530

    final changes in quick sort

--- quick_sort.c
+++ quick_sort.c
@@ -7,54 +7,52 @@ void quickSort(int numbers[], int array_size);
 void q_sort(int numbers[], int left, int right);
 void display(int a[], int n)
 {
-    int i;
-    printf("The Sorted list of numbers is :");
-    for (i=0;i<n;++i)
-    {
-        printf("%d",a[i]);
-    }
+int i;
+printf("\n\t\tThe Sorted list of numbers is : \n");
+for (i=0;i<n;++i)
+    printf("\t%d",a[i]);
 }
 
 void q_sort(int a[], int left, int right)
 {
-    int pivot, lhold, rhold;
-    lhold = left;
-    rhold = right;
-    pivot = a[left];
-    while(left<right)
-    {
-        while((a[right]>=pivot) && (left<right))
+int pivot, lhold, rhold;
+lhold = left;
+rhold = right;
+pivot = a[left];
+while(left<right)
+{
+    while((a[right]>=pivot) && (left<right))
+        right-- ;
+        if(left!=right)
+        {
+            a[left] = a[right];
+            left++ ;
+        }
+
+    while((a[left]<=pivot) && (left<right))
+        left ++ ;
+        if(left!=right)
+        {
+            a[right] = a[left];
             right-- ;
-            if(left!=right)
-            {
-                a[left] = a[right];
-                left++ ;
-            }
 
-        while((a[left]>=pivot) && (left<right))
-            left ++ ;
-            if(left!=right)
-            {
-                a[right] = a[left];
-                right-- ;
+        }
+}
 
-            }
-    }
-
-    a[left] =  pivot ;
-    pivot   =  left  ;
-    left    =  lhold ;
-    right   =  rhold ;
-    if (left < pivot)
-        q_sort(a, left, pivot-1);
-    if (right > pivot)
-        q_sort(a, pivot+1, right);
+a[left] =  pivot ;
+pivot   =  left  ;
+left    =  lhold ;
+right   =  rhold ;
+if (left < pivot)
+    q_sort(a, left, pivot-1);
+if (right > pivot)
+    q_sort(a, pivot+1, right);
 
     
 }
 void main()
 {
-    int i, n, a[100];
+    int i, n, a[100],ch;
     printf("Enter the number of elements:");
     scanf("%d", &n);
     printf("Enter the elements:");
