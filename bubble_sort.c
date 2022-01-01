20d5afe0f77e97623a6a457e42901b06e2a12119
ishita <ishitagops12@gmail.com>
2022-01-01 11:50:07 +0530

    changes made to bubble sort

--- bubble_sort.c
+++ bubble_sort.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-  int array[100], n, i, j, swap;
+  int a[100], n, i, j, temp;
 
   printf("Enter number of elements\n");
   scanf("%d", &n);
@@ -10,17 +10,17 @@ int main()
   printf("Enter %d integers\n", n);
 
   for (i = 0; i < n; i++)
-    scanf("%d", &array[i]);
+    scanf("%d", &a[i]);
 
   for (i = 0 ; i < n - 1; i++)
   {
     for (j = 0 ; j < n - i - 1; j++)
     {
-      if (array[j] > array[j+1]) /* For decreasing order use '<' instead of '>' */
+      if (a[j] > a[j+1]) /* For decreasing order use '<' instead of '>' */
       {
-        swap       = array[j];
-        array[j]   = array[j+1];
-        array[j+1] = swap;
+        temp   = a[j];
+        a[j]   = a[j+1];
+        a[j+1] = temp;
       }
     }
   }
@@ -28,7 +28,7 @@ int main()
   printf("Sorted list in ascending order:\n");
 
   for (i = 0; i < n; i++)
-     printf("%d\n", array[i]);
+     printf("%d\n", a[i]);
 
   return 0;
 }
