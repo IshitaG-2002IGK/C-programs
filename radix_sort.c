8f9b9016f70f93c7b8a36196c67845071a846c8a
ishita <ishitagops12@gmail.com>
2022-01-02 18:38:18 +0530

    radix sorting code

--- radix_sort.c
+++ radix_sort.c
@@ -4,55 +4,61 @@
 
 #include<stdio.h>
  
-int getMax(int arr[], int n) {
-    int mx = arr[0];
+int getMax(int a[], int n)
+{
+    int max = a[0];
     int i;
     for (i = 1; i < n; i++)
-        if (arr[i] > mx)
-            mx = arr[i];
-    return mx;
+        if (a[i] > max)
+            max = a[i];
+    return max;
 }
  
-void countSort(int arr[], int n, int exp) {
-    int output[n]; // output array
+void countSort(int a[], int n, int exp)
+{
+    int output[n]; // output aay
     int i, count[10] = { 0 };
  
     // Store count of occurrences in count[]
     for (i = 0; i < n; i++)
-        count[(arr[i] / exp) % 10]++;
+        count[(a[i] / exp) % 10]++;
  
     for (i = 1; i < 10; i++)
         count[i] += count[i - 1];
  
-    // Build the output array
-    for (i = n - 1; i >= 0; i--) {
-        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-        count[(arr[i] / exp) % 10]--;
+    // Build the output aay
+    for (i = n - 1; i >= 0; i--)
+    {
+        output[count[(a[i] / exp) % 10] - 1] = a[i];
+        count[(a[i] / exp) % 10]--;
     }
  
     for (i = 0; i < n; i++)
-        arr[i] = output[i];
+        a[i] = output[i];
 }
  
-// The main function to that sorts arr[] of size n using Radix Sort
-void radixsort(int arr[], int n) {
-    int m = getMax(arr, n);
+// The main function to that sorts a[] of size n using Radix Sort
+void radixsort(int a[], int n)
+{
+    int m = getMax(a, n);
  
     int exp;
     for (exp = 1; m / exp > 0; exp *= 10)
-        countSort(arr, n, exp);
+        countSort(a, n, exp);
 }
  
-void print(int arr[], int n) {
+void print(int a[], int n)
+{
     int i;
     for (i = 0; i < n; i++)
-        printf("%d ", arr[i]);
+        printf("%d ", a[i]);
 }
  
-int main() {
-    int arr[] = { 170, 45, 75, 90, 802, 24, 2, 66 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-    radixsort(arr, n);
-    print(arr, n);
+int main() 
+{
+    int a[] = { 170, 45, 75, 90, 802, 24, 2, 66 };
+    int n = sizeof(a) / sizeof(a[0]);
+    radixsort(a, n);
+    print(a, n);
     return 0;
 }
